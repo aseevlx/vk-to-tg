85f6ec1d3d8490f761c5fc7748b02045cd2ec7a7
Alexander Aseev <alexander@aseev.im>
2019-02-17 00:47:11 +0300

    add dotenv, update README

--- settings.py
+++ settings.py
@@ -1,12 +1,16 @@
+import os
 import logging
 
-BOT_API_TOKEN = ''  # token from @BotFather
-TG_CHAT_ID = ''  # telegram group name
+from dotenv import load_dotenv
 
-VK_APP_ID = 0  # vk app id
-VK_SERVICE_TOKEN = ''  # vk service app token
-VK_PAGE_ID = ''  # vk group id for tracking
-POSTS_COUNT = 20  # count of posts will fetched by every iteration
+load_dotenv()
+
+BOT_API_TOKEN = os.getenv('BOT_API_TOKEN')
+TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+VK_APP_ID = int(os.getenv('VK_APP_ID'))
+VK_SERVICE_TOKEN = os.getenv('VK_SERVICE_TOKEN')
+VK_PAGE_ID = os.getenv('VK_PAGE_ID')
+POSTS_COUNT = int(os.getenv('POSTS_COUNT'))
 
 PARSING_INTERVAL = 5*60  # time to sleep after every iteration
 
